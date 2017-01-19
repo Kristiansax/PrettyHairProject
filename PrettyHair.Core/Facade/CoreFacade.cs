f5f05cf79c0d4fb9b748a6492297ea3b2e0e0faf
Marsunbi <marsunbi@gmail.com>
2017-01-19 13:45:09 +0100

    Added facades

---
+++ PrettyHair.Core/Facade/CoreFacade.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrettyHair.Core.Entities;
+using PrettyHair.Core.Interfaces;
+using PrettyHair.Core.Repositories;
+
+namespace PrettyHair.Core.Facade
+{
+    [Serializable]
+    public class CoreFacade
+    {
+        private static volatile CoreFacade instance;
+
+        public static CoreFacade Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new CoreFacade();
+                    {
+
+                    }
+                }
+                return instance;
+            }
+        }
+        private CoreFacade()
+        {
+        }
+
+        CustomerRepository CustomerRepo = new CustomerRepository();
+        ItemRepository ItemRepo = new ItemRepository();
+        OrderlineRepository OrderlineRepo = new OrderlineRepository();
+        OrderRepository OrderRepo = new OrderRepository();
+
+        public void CreateCustomer()
+        {
+            ICustomer abemand;
+            CustomerRepo.CreateCustomer(abemandx);
+        }
+
+    }
+}
---
+++ PrettyHair.DAL/DALFacade.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrettyHair.DAL.Facade
+{
+    class DALFacade
+    {
+        private static volatile DALFacade instance;
+
+        public static DALFacade Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DALFacade();
+                }
+
+                return instance;
+            }
+        }
+
+        private DALFacade()
+        {
+
+        }
+    }
+}
